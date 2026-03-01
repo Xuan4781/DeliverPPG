98bc2584e2c941ddc946b30f9c92bf79622ab30e
Xuan4781 <123594280+Xuan4781@users.noreply.github.com>
2026-03-01 14:00:54 -0500

    Add player movement and old input system

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private CharacterController controller;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * horizontal + transform.forward * vertical;
+        controller.Move(move * moveSpeed * Time.deltaTime);
+    }
+}
